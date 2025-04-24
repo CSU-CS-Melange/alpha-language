b6b4cc52565bec6857d3a0bc5bdc1aa93ec05d17
Tani Cath <tanicath@gmail.com>
2025-04-24 00:23:35 -0600

    Base files for testing external functions.

---
+++ bundles/alpha.abft.visitor/resources/external/compiled/ext-wrapper.c
@@ -0,0 +1,228 @@
+// This file is generated from test alphabets program by code generator in alphaz
+// To compile this code, use -lm option for math library.
+
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+#include <float.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/errno.h>
+
+
+// Common Macros
+#define max(x, y)   ((x)>(y) ? (x) : (y))
+#define MAX(x, y)	((x)>(y) ? (x) : (y))
+#define min(x, y)   ((x)>(y) ? (y) : (x))
+#define MIN(x, y)	((x)>(y) ? (y) : (x))
+#define CEILD(n,d)  (int)ceil(((double)(n))/((double)(d)))
+#define ceild(n,d)  (int)ceil(((double)(n))/((double)(d)))
+#define FLOORD(n,d) (int)floor(((double)(n))/((double)(d)))
+#define floord(n,d) (int)floor(((double)(n))/((double)(d)))
+#define CDIV(x,y)    CEILD((x),(y))
+#define div(x,y)    CDIV((x),(y))
+#define FDIV(x,y)    FLOORD((x),(y))
+#define LB_SHIFT(b,s)  ((int)ceild(b,s) * s)
+#define MOD(i,j)   ((i)%(j))
+#define mallocCheck(v,s,d) if ((v) == NULL) { printf("Failed to allocate memory for %s : size=%lu\n", "sizeof(d)*(s)", sizeof(d)*(s)); exit(-1); }
+#define EPSILON 1.0E-4
+
+
+
+
+
+
+
+//Memory Macros
+
+#define b_verify() b_verify
+#define c_verify() c_verify
+#define var_b() b
+#define var_b_verify() b_verify
+#define var_c() c
+#define var_c_verify() c_verify
+
+//function prototypes
+void ext(long, float*, float*, float*);
+void ext_verify(long, float*, float*, float*);
+
+//main
+int main(int argc, char** argv) {
+	//Check number of args
+	if (argc <= 1) {
+		printf("Number of argument is smaller than expected.\n");
+		printf("Expecting N\n");
+		exit(0);
+	}
+	
+	char *end = 0;
+	char *val = 0;
+	//Read Parameters
+	//Initialisation of N
+	errno = 0;
+	end = 0;
+	val = argv[1];
+	long N = strtol(val,&end,10);
+	if ((errno == ERANGE && (N == LONG_MAX || N == LONG_MIN)) || (errno != 0 && N == 0)) {
+		perror("strtol");
+		exit(EXIT_FAILURE);
+	}
+	if (end == val) {
+		fprintf(stderr, "No digits were found for N\n");
+		exit(EXIT_FAILURE);
+	}
+	if (*end != '\0'){
+		printf("For parameter N: Converted part: %ld, non-convertible part: %s\n", N, end);
+		exit(EXIT_FAILURE);
+	}
+	
+	
+	///Parameter checking
+	if (!((N >= 1))) {
+		printf("The value of parameters are not valid.\n");
+		exit(-1);
+	}
+	
+	
+	//Memory Allocation
+	float a;
+	float b;
+	float c;
+	#ifdef VERIFY
+		float b_verify;
+		float c_verify;
+	#endif
+
+	//Initialization of rand
+	srand((unsigned)time(NULL));
+	 
+	//Input Initialization
+	{
+		#if defined (RANDOM)
+			#define S0() (a = rand()) 
+		#elif defined (CHECKING) || defined (VERIFY)
+			#ifdef NO_PROMPT
+				#define S0() scanf("%f", &a)
+			#else
+				#define S0() printf("a="); scanf("%f", &a)
+			#endif
+		#else
+			#define S0() (a = 1)   //Default value
+		#endif
+		
+		
+		S0();
+		#undef S0
+	}
+	
+	//Timing
+	struct timeval time;
+	double elapsed_time;
+	
+	//Call the main computation
+	gettimeofday(&time, NULL);
+	elapsed_time = (((double) time.tv_sec) + ((double) time.tv_usec)/1000000);
+	
+	ext(N, &a, &b, &c);
+
+	gettimeofday(&time, NULL);
+	elapsed_time = (((double) time.tv_sec) + ((double) time.tv_usec)/1000000) - elapsed_time;
+
+	// timing information
+	printf("Execution time : %lf sec.\n", elapsed_time);
+	
+	#ifdef TIMING
+		FILE * fp = fopen( "trace.dat","a+");
+		if (fp == NULL) {
+				printf("I couldn't open trace.dat for writing.\n");
+				exit(EXIT_FAILURE);
+		}
+		fprintf(fp, "%ld\t%lf\n",N,elapsed_time);
+		fclose(fp);
+	#endif
+	
+	//Verification Run
+	#ifdef VERIFY
+		#ifdef TIMING
+			gettimeofday(&time, NULL);
+			elapsed_time = (((double) time.tv_sec) + ((double) time.tv_usec)/1000000);
+		#endif
+    	ext_verify(N, &a, &b_verify, &c_verify);
+    	#ifdef TIMING
+    		gettimeofday(&time, NULL);
+			elapsed_time = (((double) time.tv_sec) + ((double) time.tv_usec)/1000000) - elapsed_time;
+			
+			FILE * fp_verify = fopen( "trace_verify.dat","a+");
+			if (fp_verify == NULL) {
+					printf("I couldn't open trace_verify.dat for writing.\n");
+					exit(EXIT_FAILURE);
+			}
+			fprintf(fp_verify, "%ld\t%lf\n",N,elapsed_time);
+			fclose(fp_verify);
+		#endif
+	#endif
+    	
+	#ifdef CHECKING
+    	//Print Outputs
+		
+		{
+			#ifdef NO_PROMPT
+				#define S0() printf("%0.2f\n",var_b())
+			#else
+				#define S0() printf("b=");printf("%0.2f\n",var_b())
+			#endif
+			S0();
+			#undef S0
+		}
+		
+		{
+			#ifdef NO_PROMPT
+				#define S0() printf("%0.2f\n",var_c())
+			#else
+				#define S0() printf("c=");printf("%0.2f\n",var_c())
+			#endif
+			S0();
+			#undef S0
+		}
+	#elif VERIFY
+		//Compare outputs for verification
+		if (fabsf(1.0f - var_b_verify()/var_b()) > EPSILON) {
+			printf("TEST for b FAILED result: %f expected: %f\n",var_b(), var_b_verify());
+		} else {
+			printf("TEST for b PASSED\n");
+		}
+		if (fabsf(1.0f - var_c_verify()/var_c()) > EPSILON) {
+			printf("TEST for c FAILED result: %f expected: %f\n",var_c(), var_c_verify());
+		} else {
+			printf("TEST for c PASSED\n");
+		}
+    #endif
+    
+	//Memory Free
+	#ifdef VERIFY
+	#endif
+	
+	return EXIT_SUCCESS;
+}
+
+//Memory Macros
+
+
+//Common Macro undefs
+#undef max
+#undef MAX
+#undef min
+#undef MIN
+#undef CEILD
+#undef ceild
+#undef FLOORD
+#undef floord
+#undef CDIV
+#undef FDIV
+#undef LB_SHIFT
+#undef MOD
+#undef EPSILON
