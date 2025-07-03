81092977acc132b9a7f28a4f90b6f0da645a7530
Campbell Bradley <jcampbellbradley@gmail.com>
2025-07-03 12:26:55 -0600

    Implement TileVerifier

---
+++ bundles/alpha.model/xtend-gen/alpha/model/tiler/TileVerifier.java
@@ -0,0 +1,28 @@
+package alpha.model.tiler;
+
+import alpha.model.AlphaSystem;
+import alpha.model.scheduler.ScheduleVerifier;
+import alpha.model.scheduler.Scheduler;
+import alpha.model.util.ISLUtil;
+import fr.irisa.cairn.jnimap.isl.ISLMap;
+import java.util.List;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
+
+@SuppressWarnings("all")
+public class TileVerifier {
+  public static void verify(final AlphaSystem sys, final Scheduler scheduler, final Tiler tiler) {
+    final List<ISLMap> maps = scheduler.getMaps().applyRange(tiler.getTileMap().toUnionMap()).getMaps();
+    ScheduleVerifier verifier = new ScheduleVerifier(maps);
+    verifier.accept(sys);
+  }
+
+  public static void verifyWavefront(final AlphaSystem sys, final Scheduler scheduler, final Tiler tiler, final int timeDims) {
+    final Function1<ISLMap, ISLMap> _function = (ISLMap it) -> {
+      return ISLUtil.convertToMultiAff(ISLUtil.toMultiAff(it).getAffs().subList(0, timeDims)).toMap();
+    };
+    final List<ISLMap> maps = ListExtensions.<ISLMap, ISLMap>map(scheduler.getMaps().applyRange(tiler.getTileMap().toUnionMap()).getMaps(), _function);
+    ScheduleVerifier verifier = new ScheduleVerifier(maps);
+    verifier.accept(sys);
+  }
+}
