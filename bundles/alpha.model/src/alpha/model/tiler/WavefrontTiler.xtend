ae96bb143153fef506376ad6d4dc1146a1daf236
Campbell Bradley <jcampbellbradley@gmail.com>
2025-07-06 14:27:10 -0600

    Change wavefront tiler to label the tiled dimensions with appropriate index names

--- bundles/alpha.model/src/alpha/model/tiler/WavefrontTiler.xtend
+++ bundles/alpha.model/src/alpha/model/tiler/WavefrontTiler.xtend
@@ -5,21 +5,41 @@ import fr.irisa.cairn.jnimap.isl.ISLSpace
 import java.util.List
 
 import static extension alpha.model.util.ISLUtil.*
+import fr.irisa.cairn.jnimap.isl.ISLAff
+import java.util.ArrayList
+import fr.irisa.cairn.jnimap.isl.ISLMap
+import alpha.model.util.ISLUtil.Dims
 
 class WavefrontTiler extends DTiler {
 	new(List<Integer> tileSizes, ISLSpace scheduleSpace, int startDim, int endDim) {
 		super(tileSizes, scheduleSpace, startDim, endDim)
 		
-		val affs = tileMap.toMultiAff.affs;
+		val nTiles = endDim-startDim+1
+		
+		val List<ISLAff> affs = tileMap.toMultiAff.affs;
 		
 		//Applies a wavefront transformation to the tile dims
-		val newAffs = #[affs.subList(0, endDim-startDim+1)
-				.reduce[a, b| a.copy.add(b.copy)]]
-			+ (0..<endDim-startDim)
+		var newAffs = new ArrayList<ISLAff>()
+		
+		newAffs += affs.subList(0, nTiles)
+				.reduce[a, b| a.copy.add(b.copy)]
+		
+		newAffs += (0..<nTiles-1)
 				.map[int dim | affs.get(dim).copy] 
-			+ affs.subList(endDim-startDim+1, affs.size)
+				
+		newAffs += affs.subList(nTiles, affs.size)
 		
 		tileMap = newAffs.toList.convertToMultiAff.toMap
+		
+		tileMap = tileMap.setDimName(Dims.OUT, 0, "tw")
+		
+		tileMap = (1..<nTiles).fold(tileMap, 
+			[ ISLMap map, i | map.setDimName(Dims.OUT, i, "t"+ i) ]
+		)
+		
+		tileMap = (nTiles..<newAffs.size).fold(tileMap, 
+			[ ISLMap map, i | map.setDimName(Dims.OUT, i, "c"+ (i-nTiles)) ]
+		)
 	}
 	
 	new(List<Integer> tileSizes, Scheduler scheduler, int startDim, int endDim) {
--- bundles/alpha.model/xtend-gen/alpha/model/tiler/WavefrontTiler.java
+++ bundles/alpha.model/xtend-gen/alpha/model/tiler/WavefrontTiler.java
@@ -4,10 +4,10 @@ import alpha.model.scheduler.Scheduler;
 import alpha.model.util.ISLUtil;
 import com.google.common.collect.Iterables;
 import fr.irisa.cairn.jnimap.isl.ISLAff;
+import fr.irisa.cairn.jnimap.isl.ISLMap;
 import fr.irisa.cairn.jnimap.isl.ISLSpace;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.ExclusiveRange;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
@@ -17,19 +17,32 @@ import org.eclipse.xtext.xbase.lib.IterableExtensions;
 public class WavefrontTiler extends DTiler {
   public WavefrontTiler(final List<Integer> tileSizes, final ISLSpace scheduleSpace, final int startDim, final int endDim) {
     super(tileSizes, scheduleSpace, startDim, endDim);
+    final int nTiles = ((endDim - startDim) + 1);
     final List<ISLAff> affs = ISLUtil.toMultiAff(this.tileMap).getAffs();
+    ArrayList<ISLAff> newAffs = new ArrayList<ISLAff>();
     final Function2<ISLAff, ISLAff, ISLAff> _function = (ISLAff a, ISLAff b) -> {
       return a.copy().add(b.copy());
     };
-    ISLAff _reduce = IterableExtensions.<ISLAff>reduce(affs.subList(0, ((endDim - startDim) + 1)), _function);
+    ISLAff _reduce = IterableExtensions.<ISLAff>reduce(affs.subList(0, nTiles), _function);
+    newAffs.add(_reduce);
     final Function1<Integer, ISLAff> _function_1 = (Integer dim) -> {
       return affs.get(dim).copy();
     };
-    Iterable<ISLAff> _map = IterableExtensions.<Integer, ISLAff>map(new ExclusiveRange(0, (endDim - startDim), true), _function_1);
-    Iterable<ISLAff> _plus = Iterables.<ISLAff>concat(Collections.<ISLAff>unmodifiableList(CollectionLiterals.<ISLAff>newArrayList(_reduce)), _map);
-    List<ISLAff> _subList = affs.subList(((endDim - startDim) + 1), affs.size());
-    final Iterable<ISLAff> newAffs = Iterables.<ISLAff>concat(_plus, _subList);
+    Iterable<ISLAff> _map = IterableExtensions.<Integer, ISLAff>map(new ExclusiveRange(0, (nTiles - 1), true), _function_1);
+    Iterables.<ISLAff>addAll(newAffs, _map);
+    List<ISLAff> _subList = affs.subList(nTiles, affs.size());
+    Iterables.<ISLAff>addAll(newAffs, _subList);
     this.tileMap = ISLUtil.convertToMultiAff(IterableExtensions.<ISLAff>toList(newAffs)).toMap();
+    this.tileMap = this.tileMap.setDimName(ISLUtil.Dims.OUT, 0, "tw");
+    final Function2<ISLMap, Integer, ISLMap> _function_2 = (ISLMap map, Integer i) -> {
+      return map.setDimName(ISLUtil.Dims.OUT, (i).intValue(), ("t" + i));
+    };
+    this.tileMap = IterableExtensions.<Integer, ISLMap>fold(new ExclusiveRange(1, nTiles, true), this.tileMap, _function_2);
+    int _size = newAffs.size();
+    final Function2<ISLMap, Integer, ISLMap> _function_3 = (ISLMap map, Integer i) -> {
+      return map.setDimName(ISLUtil.Dims.OUT, (i).intValue(), ("c" + Integer.valueOf(((i).intValue() - nTiles))));
+    };
+    this.tileMap = IterableExtensions.<Integer, ISLMap>fold(new ExclusiveRange(nTiles, _size, true), this.tileMap, _function_3);
   }
 
   public WavefrontTiler(final List<Integer> tileSizes, final Scheduler scheduler, final int startDim, final int endDim) {
