91e163b5442717ae95ad74be467d90823215e35e
Campbell Bradley <jcampbellbradley@gmail.com>
2026-05-04 13:32:32 -0600

    Many changes to HybridScheduler

--- bundles/alpha.model/src/alpha/model/scheduler/HybridScheduler.xtend
+++ bundles/alpha.model/src/alpha/model/scheduler/HybridScheduler.xtend
@@ -2,6 +2,9 @@ package alpha.model.scheduler
 
 import alpha.model.AlphaSystem
 import alpha.model.prdg.PRDG
+import alpha.model.prdg.PRDGEdge
+import fr.irisa.cairn.jnimap.isl.ISLAff
+import fr.irisa.cairn.jnimap.isl.ISLMap
 import fr.irisa.cairn.jnimap.isl.ISLSchedule
 import fr.irisa.cairn.jnimap.isl.ISLSchedule.JNIISLSchedulingOptions
 import fr.irisa.cairn.jnimap.isl.ISLUnionMap
@@ -10,6 +13,9 @@ import fr.irisa.cairn.jnimap.isl.ISLUnionSet
 import static extension alpha.model.util.ISLUtil.*
 
 class HybridScheduler extends Scheduler {
+	Iterable<ISLMap> feautrierMaps
+	Iterable<ISLMap> plutoMaps
+	int timeDims
 	ISLUnionMap spacetimeMap
 	ISLUnionSet domains
 	PRDG prdg
@@ -20,26 +26,63 @@ class HybridScheduler extends Scheduler {
 	}
 	
 	def generateSchedule(AlphaSystem system) {
-		var ISLUnionSet islDomains = this.prdg.generateDomains
-		val islPRDG = this.prdg.generateISLPRDG
-		val foutrierSchedule = ISLSchedule.computeSchedule(islDomains.copy, islPRDG.copy, JNIISLSchedulingOptions.ISL_SCHEDULE_ALGORITHM_FEAUTRIER)
+		var ISLUnionSet islDomains = prdg.generateDomains
+		val islPRDG = prdg.generateISLPRDG
+		val feautrierSchedule = ISLSchedule.computeSchedule(islDomains.copy, islPRDG.copy, JNIISLSchedulingOptions.ISL_SCHEDULE_ALGORITHM_FEAUTRIER)
 		val plutoSchedule = ISLSchedule.computeSchedule(islDomains, islPRDG, JNIISLSchedulingOptions.ISL_SCHEDULE_ALGORITHM_ISL)
-		domains = foutrierSchedule.domain
 		
-		val timeDims = system.countTimeDimensions(foutrierSchedule.map)
+		feautrierMaps = feautrierSchedule.map.maps
+		plutoMaps = plutoSchedule.map.maps
+		domains = feautrierSchedule.domain
+		timeDims = system.countTimeDimensions(feautrierSchedule.map)
 		
-		val varNames = foutrierSchedule.map.maps.map[inputTupleName]
-		spacetimeMap = varNames.map[name |
-			val timeAffs = foutrierSchedule.map.maps.findFirst[inputTupleName == name]
-				.toMultiAff.affs
-				.subList(0, timeDims)
-			val spaceAffs = plutoSchedule.map.maps.findFirst[inputTupleName == name]
-				.toMultiAff.affs
-				
-			return (timeAffs + spaceAffs)
-				.toList.convertToMultiAff
-				.toMap.setInputTupleName(name)
-		].toList.convertToUnionMap
+		spacetimeMap = feautrierMaps.map[inputTupleName]
+			.map[generateHybridMap]
+			.toList.convertToUnionMap
+	}
+	
+	def private ISLMap generateHybridMap(String name) {
+		val isReduction = prdg.getNode(name).isReductionNode
+		
+		val dominantEdge = isReduction ? prdg.dominantDependence(name) : null
+		val mapName = isReduction ? dominantEdge.dest.name : name
+		val scheduleOffset = isReduction ? 1 : 0
+			
+		var timeAffs = feautrierMaps.findFirst[inputTupleName == mapName].copy.clearInputTupleName
+			.toMultiAff.affs.subList(0, timeDims)
+		
+		if(isReduction) {
+			timeAffs = timeAffs.convertToMultiAff.pullback(dominantEdge.map.toMultiAff).affs
+		}
+
+		val spaceAffs = plutoMaps.findFirst[inputTupleName == name].copy.clearInputTupleName
+			.toMultiAff.affs
+		
+		val hybridAffs = timeAffs
+			+ #[ISLAff.buildValOnDomain(domains.sets.findFirst[tupleName == name].copy.clearTupleName.space.toLocalSpace, scheduleOffset)]
+			+ spaceAffs
+		
+		var hybridMap = hybridAffs.toList.convertToMultiAff.toMap
+		
+			
+		return hybridMap
+			.setInputTupleName(name)
+	}
+	
+	def private PRDGEdge dominantDependence(PRDG prdg, String name) {
+		prdg.edges.filter[source.name == name]
+			.reduce[a, b | dominantEdge(a,b)]
+	}
+	
+	def private PRDGEdge dominantEdge(PRDGEdge a, PRDGEdge b) {
+		val sourceSet = a.source.domain
+		val timestampA = feautrierMaps.findFirst[inputTupleName == a.dest.name].copy.clearInputTupleName
+		val timestampB = feautrierMaps.findFirst[inputTupleName == b.dest.name].copy.clearInputTupleName
+		
+		val availA = a.map.applyRange(timestampA).toMultiAff
+		val availB = b.map.applyRange(timestampB).toMultiAff
+		
+		return sourceSet.isSubset(buildLexGESet(availA, availB)) ? a : b
 	}
 	
 	override ISLUnionMap getMaps() {
--- bundles/alpha.model/xtend-gen/alpha/model/scheduler/HybridScheduler.java
+++ bundles/alpha.model/xtend-gen/alpha/model/scheduler/HybridScheduler.java
@@ -2,21 +2,32 @@ package alpha.model.scheduler;
 
 import alpha.model.AlphaSystem;
 import alpha.model.prdg.PRDG;
+import alpha.model.prdg.PRDGEdge;
 import alpha.model.util.ISLUtil;
 import com.google.common.base.Objects;
 import com.google.common.collect.Iterables;
 import fr.irisa.cairn.jnimap.isl.ISLAff;
 import fr.irisa.cairn.jnimap.isl.ISLMap;
+import fr.irisa.cairn.jnimap.isl.ISLMultiAff;
 import fr.irisa.cairn.jnimap.isl.ISLSchedule;
+import fr.irisa.cairn.jnimap.isl.ISLSet;
 import fr.irisa.cairn.jnimap.isl.ISLUnionMap;
 import fr.irisa.cairn.jnimap.isl.ISLUnionSet;
+import java.util.Collections;
 import java.util.List;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.Functions.Function2;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
-import org.eclipse.xtext.xbase.lib.ListExtensions;
 
 @SuppressWarnings("all")
 public class HybridScheduler extends Scheduler {
+  private Iterable<ISLMap> feautrierMaps;
+
+  private Iterable<ISLMap> plutoMaps;
+
+  private int timeDims;
+
   private ISLUnionMap spacetimeMap;
 
   private ISLUnionSet domains;
@@ -33,30 +44,105 @@ public class HybridScheduler extends Scheduler {
     {
       ISLUnionSet islDomains = this.prdg.generateDomains();
       final ISLUnionMap islPRDG = this.prdg.generateISLPRDG();
-      final ISLSchedule foutrierSchedule = ISLSchedule.computeSchedule(islDomains.copy(), islPRDG.copy(), ISLSchedule.JNIISLSchedulingOptions.ISL_SCHEDULE_ALGORITHM_FEAUTRIER);
+      final ISLSchedule feautrierSchedule = ISLSchedule.computeSchedule(islDomains.copy(), islPRDG.copy(), ISLSchedule.JNIISLSchedulingOptions.ISL_SCHEDULE_ALGORITHM_FEAUTRIER);
       final ISLSchedule plutoSchedule = ISLSchedule.computeSchedule(islDomains, islPRDG, ISLSchedule.JNIISLSchedulingOptions.ISL_SCHEDULE_ALGORITHM_ISL);
-      this.domains = foutrierSchedule.getDomain();
-      final int timeDims = ISLUtil.countTimeDimensions(system, foutrierSchedule.getMap());
+      this.feautrierMaps = feautrierSchedule.getMap().getMaps();
+      this.plutoMaps = plutoSchedule.getMap().getMaps();
+      this.domains = feautrierSchedule.getDomain();
+      this.timeDims = ISLUtil.countTimeDimensions(system, feautrierSchedule.getMap());
       final Function1<ISLMap, String> _function = (ISLMap it) -> {
         return it.getInputTupleName();
       };
-      final List<String> varNames = ListExtensions.<ISLMap, String>map(foutrierSchedule.getMap().getMaps(), _function);
-      final Function1<String, ISLMap> _function_1 = (String name) -> {
-        final Function1<ISLMap, Boolean> _function_2 = (ISLMap it) -> {
-          String _inputTupleName = it.getInputTupleName();
-          return Boolean.valueOf(Objects.equal(_inputTupleName, name));
-        };
-        final List<ISLAff> timeAffs = ISLUtil.toMultiAff(IterableExtensions.<ISLMap>findFirst(foutrierSchedule.getMap().getMaps(), _function_2)).getAffs().subList(0, timeDims);
-        final Function1<ISLMap, Boolean> _function_3 = (ISLMap it) -> {
-          String _inputTupleName = it.getInputTupleName();
-          return Boolean.valueOf(Objects.equal(_inputTupleName, name));
-        };
-        final List<ISLAff> spaceAffs = ISLUtil.toMultiAff(IterableExtensions.<ISLMap>findFirst(plutoSchedule.getMap().getMaps(), _function_3)).getAffs();
-        return ISLUtil.convertToMultiAff(IterableExtensions.<ISLAff>toList(Iterables.<ISLAff>concat(timeAffs, spaceAffs))).toMap().<ISLMap>setInputTupleName(name);
+      final Function1<String, ISLMap> _function_1 = (String it) -> {
+        return this.generateHybridMap(it);
       };
-      _xblockexpression = this.spacetimeMap = ISLUtil.convertToUnionMap(IterableExtensions.<ISLMap>toList(ListExtensions.<String, ISLMap>map(varNames, _function_1)));
+      _xblockexpression = this.spacetimeMap = ISLUtil.convertToUnionMap(IterableExtensions.<ISLMap>toList(IterableExtensions.<String, ISLMap>map(IterableExtensions.<ISLMap, String>map(this.feautrierMaps, _function), _function_1)));
     }
     return _xblockexpression;
+  }
+
+  private ISLMap generateHybridMap(final String name) {
+    final boolean isReduction = this.prdg.getNode(name).isReductionNode();
+    PRDGEdge _xifexpression = null;
+    if (isReduction) {
+      _xifexpression = this.dominantDependence(this.prdg, name);
+    } else {
+      _xifexpression = null;
+    }
+    final PRDGEdge dominantEdge = _xifexpression;
+    String _xifexpression_1 = null;
+    if (isReduction) {
+      _xifexpression_1 = dominantEdge.getDest().getName();
+    } else {
+      _xifexpression_1 = name;
+    }
+    final String mapName = _xifexpression_1;
+    int _xifexpression_2 = (int) 0;
+    if (isReduction) {
+      _xifexpression_2 = 1;
+    } else {
+      _xifexpression_2 = 0;
+    }
+    final int scheduleOffset = _xifexpression_2;
+    final Function1<ISLMap, Boolean> _function = (ISLMap it) -> {
+      String _inputTupleName = it.getInputTupleName();
+      return Boolean.valueOf(Objects.equal(_inputTupleName, mapName));
+    };
+    List<ISLAff> timeAffs = ISLUtil.toMultiAff(IterableExtensions.<ISLMap>findFirst(this.feautrierMaps, _function).copy().clearInputTupleName()).getAffs().subList(0, this.timeDims);
+    if (isReduction) {
+      timeAffs = ISLUtil.convertToMultiAff(timeAffs).pullback(ISLUtil.toMultiAff(dominantEdge.getMap())).getAffs();
+    }
+    final Function1<ISLMap, Boolean> _function_1 = (ISLMap it) -> {
+      String _inputTupleName = it.getInputTupleName();
+      return Boolean.valueOf(Objects.equal(_inputTupleName, name));
+    };
+    final List<ISLAff> spaceAffs = ISLUtil.toMultiAff(IterableExtensions.<ISLMap>findFirst(this.plutoMaps, _function_1).copy().clearInputTupleName()).getAffs();
+    final Function1<ISLSet, Boolean> _function_2 = (ISLSet it) -> {
+      String _tupleName = it.getTupleName();
+      return Boolean.valueOf(Objects.equal(_tupleName, name));
+    };
+    ISLAff _buildValOnDomain = ISLAff.buildValOnDomain(IterableExtensions.<ISLSet>findFirst(this.domains.getSets(), _function_2).copy().clearTupleName().getSpace().toLocalSpace(), scheduleOffset);
+    Iterable<ISLAff> _plus = Iterables.<ISLAff>concat(timeAffs, Collections.<ISLAff>unmodifiableList(CollectionLiterals.<ISLAff>newArrayList(_buildValOnDomain)));
+    final Iterable<ISLAff> hybridAffs = Iterables.<ISLAff>concat(_plus, spaceAffs);
+    ISLMap hybridMap = ISLUtil.convertToMultiAff(IterableExtensions.<ISLAff>toList(hybridAffs)).toMap();
+    return hybridMap.<ISLMap>setInputTupleName(name);
+  }
+
+  private PRDGEdge dominantDependence(final PRDG prdg, final String name) {
+    final Function1<PRDGEdge, Boolean> _function = (PRDGEdge it) -> {
+      String _name = it.getSource().getName();
+      return Boolean.valueOf(Objects.equal(_name, name));
+    };
+    final Function2<PRDGEdge, PRDGEdge, PRDGEdge> _function_1 = (PRDGEdge a, PRDGEdge b) -> {
+      return this.dominantEdge(a, b);
+    };
+    return IterableExtensions.<PRDGEdge>reduce(IterableExtensions.<PRDGEdge>filter(prdg.getEdges(), _function), _function_1);
+  }
+
+  private PRDGEdge dominantEdge(final PRDGEdge a, final PRDGEdge b) {
+    final ISLSet sourceSet = a.getSource().getDomain();
+    final Function1<ISLMap, Boolean> _function = (ISLMap it) -> {
+      String _inputTupleName = it.getInputTupleName();
+      String _name = a.getDest().getName();
+      return Boolean.valueOf(Objects.equal(_inputTupleName, _name));
+    };
+    final ISLMap timestampA = IterableExtensions.<ISLMap>findFirst(this.feautrierMaps, _function).copy().clearInputTupleName();
+    final Function1<ISLMap, Boolean> _function_1 = (ISLMap it) -> {
+      String _inputTupleName = it.getInputTupleName();
+      String _name = b.getDest().getName();
+      return Boolean.valueOf(Objects.equal(_inputTupleName, _name));
+    };
+    final ISLMap timestampB = IterableExtensions.<ISLMap>findFirst(this.feautrierMaps, _function_1).copy().clearInputTupleName();
+    final ISLMultiAff availA = ISLUtil.toMultiAff(a.getMap().applyRange(timestampA));
+    final ISLMultiAff availB = ISLUtil.toMultiAff(b.getMap().applyRange(timestampB));
+    PRDGEdge _xifexpression = null;
+    boolean _isSubset = sourceSet.isSubset(ISLUtil.buildLexGESet(availA, availB));
+    if (_isSubset) {
+      _xifexpression = a;
+    } else {
+      _xifexpression = b;
+    }
+    return _xifexpression;
   }
 
   @Override
